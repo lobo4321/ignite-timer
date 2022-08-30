2f59fc25ca9e0074754e60a5d7bf5fa15f05b781
Joao Lobo <jglobo47@gmail.com>
2022-08-30 14:49:36 -0300

    refactor: contenct and reducer

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,10 +1,17 @@
-import { createContext, ReactNode, useState, useReducer } from "react";
+import { differenceInSeconds } from "date-fns";
+import {
+  createContext,
+  ReactNode,
+  useState,
+  useReducer,
+  useEffect,
+} from "react";
 import {
   addNewCycleAction,
   markCurrentCycleAsFinishedAction,
   interruptCurrentCycleAction,
 } from "../reducers/cycles/actions";
-import { Cycle, cyclesReducer } from "../reducers/cycles/reducer";
+import { Cycle, cyclesReducer } from "../reducers/cycles/reducer"; // eslint-disable-line
 
 interface CreateCycleData {
   task: string;
@@ -31,16 +38,40 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  });
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    () => {
+      const storageStateAsJSON = localStorage.getItem(
+        "ignite-timer:cycles-state-1.0.0"
+      );
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+      if (storageStateAsJSON) {
+        return JSON.parse(storageStateAsJSON);
+      }
+    }
+  );
 
   const { cycles, activeCycleId } = cyclesState;
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate));
+    }
+
+    return 0;
+  });
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState);
+
+    localStorage.setItem("ignite-timer:cycles-state-1.0.0", stateJSON); // eslint-disable-line
+  }, [cyclesState]);
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds);
